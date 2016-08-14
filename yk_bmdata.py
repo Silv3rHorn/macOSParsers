9745c1d32323d89096383b08f20e86f46acf4c3d
wcyk <wcyk@fake-email.com>
2016-08-14 21:04:35 +0800

    Updated bookmark parser and grammar to parse more record types

--- yk_bmdata.py
+++ yk_bmdata.py
@@ -1,10 +1,38 @@
 import argparse
+import datetime
 import pprint
 import struct
 
 __author__ = 'yk'
-__version__ = 'v.20160723'
-__reference__ = 'http://michaellynn.github.io/2015/10/24/apples-bookmarkdata-exposed/'
+__version__ = 'v.20160810'
+__reference1__ = "http://michaellynn.github.io/2015/10/24/apples-bookmarkdata-exposed/"
+__reference2__ = "Simon Key - Mac OS X - Delving a Little Deeper"
+
+RecordTypes = {
+    4100: 'filePath',  # pathComponents
+    4101: 'fileInodePath',  # fileIDs
+    4112: 'resourceProps',
+    4160: 'fileCreationDate',  # creationDate
+    8192: 'volInfoDepths',
+    8194: 'volPath',
+    8197: 'volURL',
+    8208: 'volName',
+    8209: 'volUUID',
+    8210: 'volSize',  # volCapacity
+    8211: 'volCreationDate',
+    8224: 'volProps',
+    8240: 'volWasBoot',
+    8272: 'volMountURL',
+    49153: 'volDepthCountHome',  # Vol Home Dir Relative Path Component Count
+    49169: 'username',
+    49170: 'userUID',
+    53264: 'creationOptions',
+    61463: 'displayName',
+    61473: 'iconRef',  # Effective Flattened Icon Ref
+    61488: 'bookmarkCreationDate',  # Bookmark Creation Time
+    61568: 'sandboxInfo'  # Sandbox RW Extension
+}
+Cocoa_to_Epoch = 978307200
 
 
 class ToCRecord:
@@ -96,7 +124,15 @@ def parse(raw_data):
             bm_data.data.records[-1].data = struct.unpack(str(bm_data.data.records[-1].length) + 's',
                                                           raw_data[raw_record[2] + 8:raw_record[2] + 8 + bm_data.data.
                                                           records[-1].length])[0]
-            interpret(toc, bm_data.data, raw_data)
+            __parse_record(toc, bm_data.data, raw_data)
+
+        if "volSize" in toc:  # make volume size human-readable
+            vol_size = float(toc["volSize"])
+            for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB']:
+                if abs(vol_size) < 1024.0:
+                    toc["volSize"] = "%3.2f%s" % (vol_size, unit)
+                    break
+                vol_size /= 1024.0
 
         result.append(toc)
         if bm_data.data.ToCs[-1].data.offset_next_toc == 0:
@@ -107,95 +143,68 @@ def parse(raw_data):
     return result
 
 
-def interpret(toc, data, raw_data):
+def __parse_record(toc, data, raw_data):
     toc_record_type = data.ToCs[-1].records[-1].type
     record = data.records[-1]
+    
+    parsed_record = __parse_data_type(record, raw_data)
+    if parsed_record is None:
+        return
 
-    if toc_record_type == 4100:  # pathComponents
-        toc['filePath'] = ''
-        offsets = get_offsets(record)
+    if toc_record_type in RecordTypes:
+        toc[RecordTypes[toc_record_type]] = parsed_record
+
+
+def __parse_data_type(record, raw_data):
+    if record.type == 257:  # string
+        return record.data
+
+    elif record.type == 513:  # binary
+        return None
+
+    elif record.type == 771:  # unsigned-int (4-byte)
+        return struct.unpack('<I', record.data)[0]
+
+    elif record.type == 772:  # unsigned-int (8-byte)
+        return struct.unpack('<Q', record.data)[0]
+
+    elif record.type == 774:  # double (Date, Little-Endian)
+        raw_date = struct.unpack('<d', record.data)[0]
+        return datetime.datetime.utcfromtimestamp(raw_date + Cocoa_to_Epoch).strftime('%d %b %Y %H:%M:%S.%f')
+
+    elif record.type == 1024:  # double (Date, Big-Endian)
+        raw_date = struct.unpack('>d', record.data)[0]
+        return datetime.datetime.utcfromtimestamp(raw_date + Cocoa_to_Epoch).strftime('%d %b %Y %H:%M:%S.%f')
+
+    elif record.type == 1281:  # boolean true
+        return "True"
+
+    elif record.type == 1537:  # pointer array
+        count = record.length // 4
+        if record.length % 4 > 0:
+            count += 1
+
+        offsets = []
+        for offset in range(count):
+            offsets.append(struct.unpack('<I', record.data[0:4])[0])
+            record.data = record.data[4:]
+
+        components = ""
         for offset in offsets:
-            path_component = DataRecord()
-            path_component.length = struct.unpack('<I', raw_data[offset:offset + 4])[0]
-            path_component.type = struct.unpack('<I', raw_data[offset + 4:offset + 8])[0]
-            path_component.data = struct.unpack(str(path_component.length) + 's',
-                                                raw_data[offset + 8:offset + 8 + path_component.length])[0]
-            toc['filePath'] += '/' + path_component.data.decode('utf-8')
-        # print('filePath     : {}'.format(toc['filePath']))
+            component = DataRecord()
+            component.length = struct.unpack('<I', raw_data[offset:offset + 4])[0]
+            component.type = struct.unpack('<I', raw_data[offset + 4:offset + 8])[0]
+            component.data = struct.unpack(str(component.length) + 's',
+                                           raw_data[offset + 8:offset + 8 + component.length])[0]
+            components += '/' + str(__parse_data_type(component, raw_data))
 
-    elif toc_record_type == 4101:  # fileIDs
-        toc['fileInodePath'] = ''
-        offsets = get_offsets(record)
-        for offset in offsets:
-            inodes_component = DataRecord()
-            inodes_component.length = struct.unpack('<I', raw_data[offset:offset + 4])[0]
-            if inodes_component.length == 8:
-                inodes_component.data = struct.unpack('Q', raw_data[offset + 8:offset + 8 + 8])[0]
-                toc['fileInodePath'] += '/' + str(inodes_component.data)
-            elif inodes_component.length == 4:
-                inodes_component.data = struct.unpack('I', raw_data[offset + 8:offset + 8 + 4])[0]
-                toc['fileInodePath'] += '/' + str(inodes_component.data)
-            else:
-                toc['fileInodePath'] += '/?'
-        # print('inodePath    : {}'.format(toc['fileInodePath']))
+        return components
 
-    elif toc_record_type == 4112:  # resourceProps
-        # print('resourceProps: {}'.format('?'))
-        pass
-
-    elif toc_record_type == 4160:  # creationDate
-        # print('creationDate : {}'.format('?'))
-        pass
-
-    elif toc_record_type == 8192:  # volInfoDepths
-        # print('volInfoDepths: {}'.format('?'))
-        pass
-
-    elif toc_record_type == 8194:  # volPath
-        toc['volPath'] = record.data
-        # print('volPath      : {}'.format(toc['volPath']))
-
-    elif toc_record_type == 8197:  # volURL
-        toc['volURL'] = record.data
-        # print('volURL       : {}'.format(toc['volURL']))
-
-    elif toc_record_type == 8208:  # volName
-        toc['volName'] = record.data
-        # print('volName      : {}'.format(toc['volName']))
-
-    elif toc_record_type == 8209:  # volUUID
-        toc['volUUID'] = record.data
-        # print('volUUID      : {}'.format(toc['volUUID']))
-
-    elif toc_record_type == 8272:  # volMountURL
-        # print('volMountURL  : {}'.format('?'))
-        pass
-
-    elif toc_record_type == 49169:  # user
-        toc['user'] = record.data
-        # print('user         : {}'.format(toc['user']))
-
-    elif toc_record_type == 61568:  # sandboxInfo
-        toc['sandbox'] = record.data
-        # print('sandboxInfo  : {}'.format(toc['sandbox']))
+    elif record.type == 2305:  # CFURL
+        return record.data
 
     else:
-        # print('record type {} is currently unknown'.format(toc_record_type))
-        pass
-
-
-def get_offsets(record):
-    count = record.length // 4
-    if record.length % 4 > 0:
-        count += 1
-
-    offsets = []
-    data = record.data
-    for offset in range(count):
-        offsets.append(struct.unpack('<I', data[0:4])[0])
-        data = data[4:]
-
-    return offsets
+        return None
 
 
 result = []
