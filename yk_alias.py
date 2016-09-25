9a2dd2673e8a49739c7c514e4118cd0e4081d3a5
wcyk <wcyk@fake-email.com>
2016-09-25 21:40:06 +0800

    Consolidate alias libraries into a single library

--- yk_alias.py
+++ yk_alias.py
@@ -4,14 +4,16 @@ import pprint
 import struct
 
 __author__ = 'yk'
-__version__ = '.20160723'
+__version__ = '.20160925'
 __reference__ = 'http://cpansearch.perl.org/src/WIML/Mac-Alias-Parse-0.20/Parse.pm'
 
 
-RecordTypes = ["folderName", "parentInodePath", "filePath", "appleshare_zoneName",
-               "appleShare_serverName", "appleShare_username", "driverName", "", "", "network_mountInfo",
-               "appleRemoteAccess_dialupInfo", "", "", "", "fileName_utf16", "volName_utf16", "volCreationDate_utc",
-               "fileCreationDate_utc", "filePath_posix", "mountPoint_posix", "aliasOfDMG", "userHome_posix"]
+RecordTypesV2 = ["folderName", "parentInodePath", "filePath", "appleshare_zoneName",
+                 "appleShare_serverName", "appleShare_username", "driverName", "", "", "network_mountInfo",
+                 "appleRemoteAccess_dialupInfo", "", "", "", "fileName_utf16", "volName_utf16", "volCreationDate_utc",
+                 "fileCreationDate_utc", "filePath_posix", "mountPoint_posix", "aliasOfDMG", "userHome_posix"]
+RecordTypesV3 = ["", "parentInodePath", "", "", "", "", "", "", "", "", "", "", "", "", "fileName_utf16", "volName_utf16",
+                 "", "", "filePath", "volMountPoint"]
 HFS_to_Epoch = 2082844800
 
 
@@ -29,25 +31,25 @@ class AliasData:
         kind_item = 0  # file or folder
 
     class Volume:
-        length = 0
-        name = ""
+        length = 0  # v2 only
+        name = ""  # v2 only
         date_creation = 0  # local time
         type_fs = ''
-        type_vol = 0
+        type_vol = 0  # v2 only
 
     class Item:
         inode_pfolder = 0
-        len_filename = 0
-        name = ""
+        len_filename = 0  # v2 only
+        name = ""  # v2 only
         inode = 0
         date_creation = 0
-        creator_code = None  # ignore
-        type = None  # ignore
-        levels_from = 0  # ignore
-        levels_to = 0  # ignore
+        creator_code = None  # v2 only; ignore
+        type = None  # v2 only; ignore
+        levels_from = 0  # v2 only; ignore
+        levels_to = 0  # v2 only; ignore
 
-    vol_attr_flags = None  # ignore
-    vol_fsid = None  # ignore
+    vol_attr_flags = None  # v2 only; ignore
+    vol_fsid = None  # v2 only; ignore
 
     records = []
 
@@ -55,16 +57,47 @@ class AliasData:
 def parse(raw_data):
     result.clear()
     alias_data = AliasData()
+    alias_data.header = AliasData.Header()
+    alias_data.item = AliasData.Item()
+    alias_data.volume = AliasData.Volume()
 
     # header
-    alias_data.header = AliasData.Header()
     alias_data.header.type_user = struct.unpack('4s', raw_data[0:4])[0]
     alias_data.header.length = struct.unpack('>H', raw_data[4:6])[0]
     alias_data.header.version = struct.unpack('>H', raw_data[6:8])[0]
     alias_data.header.kind_item = struct.unpack('>H', raw_data[8:10])[0]
 
+    if alias_data.header.version == 2:
+        raw_data = __parse_v2(raw_data, alias_data)
+    elif alias_data.header.version == 3:
+        raw_data = __parse_v3(raw_data, alias_data)
+
+    alias_data.records = []
+    while True:
+        record = AliasRecord()
+        record.type = struct.unpack('>h', raw_data[0:2])[0]
+        record.length = struct.unpack('>H', raw_data[2:4])[0]
+        record.data = struct.unpack(str(record.length) + 's', raw_data[4:4 + record.length])[0]
+        alias_data.records.append(record)
+
+        raw_data = raw_data[2 + 2 + record.length:]
+        if (2 + 2 + record.length) % 2 != 0:
+            raw_data = raw_data[1:]
+        if record.type == -1:  # end of list
+            break
+
+    if alias_data.header.version == 2:
+        __interpret_v2(alias_data)
+        # __debug_v2(alias_data)
+    elif alias_data.header.version == 3:
+        __interpret_v3(alias_data)
+        # __debug_v3(alias_data)
+
+    return result
+
+
+def __parse_v2(raw_data, alias_data):
     # volume details
-    alias_data.volume = AliasData.Volume()
     alias_data.volume.length = struct.unpack('>B', raw_data[10:11])[0]
     alias_data.volume.name = struct.unpack(str(alias_data.volume.length) + 's',
                                            raw_data[11:11 + alias_data.volume.length])[0]
@@ -73,7 +106,6 @@ def parse(raw_data):
     alias_data.volume.type_vol = struct.unpack('>H', raw_data[44:46])[0]
 
     # item details
-    alias_data.item = AliasData.Item()
     alias_data.item.inode_pfolder = struct.unpack('>I', raw_data[46:50])[0]
     alias_data.item.len_filename = struct.unpack('>B', raw_data[50:51])[0]
     alias_data.item.name = struct.unpack(str(alias_data.item.len_filename) + 's',
@@ -90,27 +122,24 @@ def parse(raw_data):
     alias_data.vol_fsid = struct.unpack('2s', raw_data[138:140])[0]
 
     # records
-    alias_data.records = []
-    raw_data = raw_data[150:]
-    while True:
-        record = AliasRecord()
-        record.type = struct.unpack('>h', raw_data[0:2])[0]
-        record.length = struct.unpack('>H', raw_data[2:4])[0]
-        record.data = struct.unpack(str(record.length) + 's', raw_data[4:4 + record.length])[0]
-        alias_data.records.append(record)
-
-        raw_data = raw_data[2 + 2 + record.length:]
-        if (2 + 2 + record.length) % 2 != 0:
-            raw_data = raw_data[1:]
-        if record.type == -1:  # end of list
-            break
-
-    _interpret(alias_data)
-    # _debug(alias_data)
-    return result
+    return raw_data[150:]
 
 
-def _interpret(alias_data):
+def __parse_v3(raw_data, alias_data):
+    # volume details
+    alias_data.volume.date_creation = struct.unpack('>I', raw_data[12:16])[0]
+    alias_data.volume.type_fs = struct.unpack('2s', raw_data[18:20])[0]
+
+    # item details
+    alias_data.item.inode_pfolder = struct.unpack('>I', raw_data[24:28])[0]
+    alias_data.item.inode = struct.unpack('>I', raw_data[28:32])[0]
+    alias_data.item.date_creation = struct.unpack('>I', raw_data[34:38])[0]
+
+    # records
+    return raw_data[58:]
+
+
+def __interpret_v2(alias_data):
     if alias_data.header.kind_item == 0:
         result['fileKind'] = "file"
     elif alias_data.header.kind_item == 1:
@@ -148,28 +177,63 @@ def _interpret(alias_data):
                 inode_components.append(struct.unpack('>I', record.data[0:4])[0])
                 record.data = record.data[4:]
 
-            result[RecordTypes[1]] = ""
+            result[RecordTypesV2[1]] = ""
             for inode in reversed(inode_components):
-                result[RecordTypes[1]] += str(inode) + '/'
+                result[RecordTypesV2[1]] += str(inode) + '/'
         if record.type == 2:
-            result[RecordTypes[2]] = record.data.replace(b':', b'/')
+            result[RecordTypesV2[2]] = record.data.replace(b':', b'/')
         elif record.type == 14:
-            result[RecordTypes[14]] = (record.data[1:] + b'\x00').decode('utf-16')
+            result[RecordTypesV2[14]] = (record.data[1:] + b'\x00').decode('utf-16')
         elif record.type == 15:
-            result[RecordTypes[15]] = (record.data[1:] + b'\x00').decode('utf-16')
+            result[RecordTypesV2[15]] = (record.data[1:] + b'\x00').decode('utf-16')
         elif record.type == 16:
-            result[RecordTypes[16]] = datetime.datetime.utcfromtimestamp(
+            result[RecordTypesV2[16]] = datetime.datetime.utcfromtimestamp(
                 struct.unpack('>I', record.data[2:-2])[0] - HFS_to_Epoch).strftime('%d %b %Y %H:%M:%S')
         elif record.type == 17:
-            result[RecordTypes[17]] = datetime.datetime.utcfromtimestamp(
+            result[RecordTypesV2[17]] = datetime.datetime.utcfromtimestamp(
                 struct.unpack('>I', record.data[2:-2])[0] - HFS_to_Epoch).strftime('%d %b %Y %H:%M:%S')
 
 
-def _debug(alias_data):
+def __interpret_v3(alias_data):
+    if alias_data.header.kind_item == 0:
+        result['fileKind'] = "file"
+    elif alias_data.header.kind_item == 1:
+        result['fileKind'] = "folder"
+
+    result['volCreationDate'] = datetime.datetime.utcfromtimestamp(alias_data.volume.date_creation -
+                                                                   HFS_to_Epoch).strftime('%d %b %Y %H:%M:%S')
+    result['parentInode'] = alias_data.item.inode_pfolder
+    result['fileInode'] = alias_data.item.inode
+    result['volFileSystem'] = alias_data.volume.type_fs
+    result['fileCreationDate'] = datetime.datetime.utcfromtimestamp(alias_data.item.date_creation -
+                                                                    HFS_to_Epoch).strftime('%d %b %Y %H:%M:%S')
+
+    for record in alias_data.records:
+        if record.type == 1:
+            inode_components = []
+            count_components = int(record.length / 4)
+            for _ in range(count_components):
+                inode_components.append(struct.unpack('>I', record.data[0:4])[0])
+                record.data = record.data[4:]
+
+            result[RecordTypesV3[1]] = ""
+            for inode in reversed(inode_components):
+                result[RecordTypesV3[1]] += str(inode) + '/'
+        elif record.type == 14:
+            result[RecordTypesV3[14]] = (record.data[1:] + b'\x00').decode('utf-16')
+        elif record.type == 15:
+            result[RecordTypesV3[15]] = (record.data[1:] + b'\x00').decode('utf-16')
+        elif record.type == 18:
+            result[RecordTypesV3[18]] = record.data
+        elif record.type == 19:
+            result[RecordTypesV3[19]] = record.data
+
+
+def __debug_v2(alias_data):
     print("{}".format("===== Header ====="))
     print("Length       : {}".format(alias_data.header.length))
     print("Version      : {}".format(alias_data.header.version))
-    print("Item Kind    : {}".format(alias_data.header.kind_item) + " (0 = file, 1 = folder)")
+    print("Item Kind    : {} (0 = file, 1 = folder)".format(alias_data.header.kind_item))
 
     print("{}".format("===== Volume ====="))
     print("Length       : {}".format(alias_data.volume.length))
@@ -187,6 +251,23 @@ def _debug(alias_data):
     print("Date Creation: {}".format(alias_data.item.date_creation))
     print("Levels from  : {}".format(alias_data.item.levels_from))
     print("Levels to    : {}".format(alias_data.item.levels_to))
+    print("{}".format("================"))
+
+
+def __debug_v3(alias_data):
+    print("{}".format("===== Header ====="))
+    print("Length       : {}".format(alias_data.header.length))
+    print("Version      : {}".format(alias_data.header.version))
+    print("Item Kind    : {} (0 = file, 1 = folder)".format(alias_data.header.kind_item))
+
+    print("{}".format("===== Volume ====="))
+    print("Date Creation: {}".format(alias_data.volume.date_creation))
+    print("FS Type      : {}".format(alias_data.volume.type_fs.decode('utf-8')))
+
+    print("{}".format("===== Item ====="))
+    print("Pfolder Inode: {}".format(alias_data.item.inode_pfolder))
+    print("Inode        : {}".format(alias_data.item.inode))
+    print("Date Creation: {}".format(alias_data.item.date_creation))
     print("{}".format("================"))
 
 
